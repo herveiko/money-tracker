39decf2248df47a255145e9a9d9956492b57c8ae
herveiko <hikorineza@asystresources.com>
2024-10-28 16:21:29 +0200

    initialise project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+My App
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+  
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+        <h1>This is an about page</h1>
+  </main>
+</template>
